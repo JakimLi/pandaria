c0eb951196c7d242aefa6640091aaa6d409a129b
jakimli <ljking7002@gmail.com>
2018-10-01 11:55:25 +0800

    refactor the json verification to make it simple

--- panda-core/src/main/java/com/github/jakimli/panda/domain/verification/Verification.java
+++
@@ -1,5 +0,0 @@
-package com.github.jakimli.panda.domain.verification;
-
-public interface Verification {
-    void verify(Object item) throws Throwable;
-}
--- panda-core/src/main/java/com/github/jakimli/panda/steps/verification/JsonVerificationSteps.java
+++ panda-core/src/main/java/com/github/jakimli/panda/steps/verification/JsonVerificationSteps.java
@@ -1,9 +1,12 @@
 package com.github.jakimli.panda.steps.verification;
 
 import com.github.jakimli.panda.domain.Variables;
-import com.github.jakimli.panda.domain.verification.JsonVerificationContext;
+import com.github.jakimli.panda.domain.verification.VerificationContext;
+import com.github.jakimli.panda.utils.JsonContext;
 import cucumber.api.java.en.Then;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
 
 import static java.lang.Double.parseDouble;
 import static org.hamcrest.CoreMatchers.not;
@@ -14,82 +17,84 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 public class JsonVerificationSteps {
+
     @Autowired
-    JsonVerificationContext verifier;
+    VerificationContext toBeVerified;
 
     @Autowired
     Variables variables;
 
+    private Object json(String path) throws IOException {
+        return JsonContext.json(toBeVerified.toJsonObject()).path(path);
+    }
+
     @Then("^verify: '([^\"]*)'='([^\"]*)'$")
     public void verifyEqualsLiteral(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, is(expected)));
+        assertThat(json(path), is(expected));
     }
 
     @Then("^verify: '([^\"]*)'!='([^\"]*)'$")
     public void verifyNotEqualsLiteral(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, not(expected)));
+        assertThat(json(path), not(expected));
     }
 
     @Then("^verify: '([^\"]*)'=\"([^\"]*)\"$")
     public void verifyEqualsString(String path, String expected) throws Throwable {
-        final String interpret = variables.interpret(expected);
-        verifier.verify(path, actual -> assertThat(actual, is(interpret)));
+        assertThat(json(path), is(variables.interpret(expected)));
     }
 
     @Then("^verify: '([^\"]*)'!=\"([^\"]*)\"$")
     public void verifyNotEqualsString(String path, String expected) throws Throwable {
-        final String interpret = variables.interpret(expected);
-        verifier.verify(path, actual -> assertThat(actual, not(interpret)));
+        assertThat(json(path), not(variables.interpret(expected)));
     }
 
     @Then("^verify: '([^\"]*)' contains: '([^\"]*)'$")
     public void verifyContainsLiteral(String path, String contained) throws Throwable {
-        verifier.verify(path, actual -> assertThat(String.valueOf(actual), containsString(contained)));
+        assertThat(String.valueOf(json(path)), containsString(contained));
     }
 
     @Then("^verify: '([^\"]*)' contains: \"([^\"]*)\"$")
     public void verifyContainsString(String path, String contained) throws Throwable {
-        final String interpret = variables.interpret(contained);
-        verifier.verify(path, actual -> assertThat(String.valueOf(actual), containsString(interpret)));
+        assertThat(String.valueOf(json(path)), containsString(variables.interpret(contained)));
     }
 
     @Then("^verify: '([^\"]*)'=(\\d+)$")
     public void verifyEqualsInteger(String path, final int expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, is(expected)));
+        assertThat(json(path), is(expected));
     }
 
     @Then("^verify: '([^\"]*)'!=(\\d+)$")
     public void verifyNotEqualsInteger(String path, final int expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, not(expected)));
+        assertThat(json(path), not(expected));
     }
 
     @Then("^verify: '([^\"]*)'>(\\d+)$")
     public void verifyGreaterThanInteger(String path, final int expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat((int) actual, greaterThan(expected)));
+        assertThat((int) json(path), greaterThan(expected));
     }
 
     @Then("^verify: '([^\"]*)'<(\\d+)$")
     public void verifyLessThanInteger(String path, final int expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat((int) actual, lessThan(expected)));
+        assertThat((int) json(path), lessThan(expected));
     }
 
     @Then("^verify: '([^\"]*)'=(\\d+\\.\\d+)$")
     public void verifyEqualsDouble(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, is(parseDouble(expected))));
+        assertThat(json(path), is(parseDouble(expected)));
     }
 
     @Then("^verify: '([^\"]*)'!=(\\d+\\.\\d+)$")
     public void verifyNotEqualsDouble(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat(actual, not(parseDouble(expected))));
+        assertThat(json(path), not(parseDouble(expected)));
     }
 
     @Then("^verify: '([^\"]*)'>(\\d+\\.\\d+)$")
     public void verifyGreaterThanDouble(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat((double) actual, greaterThan(parseDouble(expected))));
+        assertThat((double) json(path), greaterThan(parseDouble(expected)));
     }
 
     @Then("^verify: '([^\"]*)'<(\\d+\\.\\d+)$")
     public void verifyLessThanDouble(String path, final String expected) throws Throwable {
-        verifier.verify(path, actual -> assertThat((double) actual, lessThan(parseDouble(expected))));
+        assertThat((double) json(path), lessThan(parseDouble(expected)));
     }
 }
