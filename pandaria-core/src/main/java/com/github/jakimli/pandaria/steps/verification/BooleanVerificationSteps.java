1b880d75174beacd254bc52c5d8e5720ee727bc2
jakimli <ljking7002@gmail.com>
2018-10-03 23:32:12 +0800

    add boolean verification from variable

--- pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/verification/BooleanVerificationSteps.java
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/verification/BooleanVerificationSteps.java
@@ -1,5 +1,6 @@
 package com.github.jakimli.pandaria.steps.verification;
 
+import com.github.jakimli.pandaria.domain.Variables;
 import com.github.jakimli.pandaria.domain.VerificationContext;
 import cucumber.api.java.en.Then;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,16 @@ public class BooleanVerificationSteps {
     @Autowired
     VerificationContext actual;
 
+    @Autowired
+    Variables variables;
+
     @Then("^verify: '([^\"]*)'=(true|false)$")
     public void verifyEqualsBoolean(String path, String expected) throws IOException {
         assertThat(actual.json(path), is(Boolean.valueOf(expected)));
+    }
+
+    @Then("^verify: \\$\\{([^\"]*)}=(true|false)$")
+    public void verifyVariableEqualsBoolean(String name, String expected) throws IOException {
+        assertThat(variables.get(name), is(Boolean.valueOf(expected)));
     }
 }
