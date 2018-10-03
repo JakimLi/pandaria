ee0e844d15747e73f4f8bde87011ab73f1d9debe
jakimli <ljking7002@gmail.com>
2018-10-03 14:48:39 +0800

    add variabel name

--- pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/verification/DateTimeVerificationSteps.java
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/verification/DateTimeVerificationSteps.java
@@ -1,5 +1,6 @@
 package com.github.jakimli.pandaria.steps.verification;
 
+import com.github.jakimli.pandaria.domain.Variables;
 import com.github.jakimli.pandaria.domain.VerificationContext;
 import cucumber.api.java.en.Then;
 import org.joda.time.DateTime;
@@ -16,8 +17,16 @@ public class DateTimeVerificationSteps {
     @Autowired
     VerificationContext actual;
 
+    @Autowired
+    Variables variables;
+
     @Then("^verify: '([^\"]*)'=datetime: '([^\"]*)' pattern: '([^\"]*)'$")
     public void verifyDateWithPattern(String path, String date, String pattern) throws IOException {
         assertThat(new DateTime(actual.json(path)), is(forPattern(pattern).parseDateTime(date)));
+    }
+
+    @Then("^verify: \\$\\{([^\"]*)}=datetime: '([^\"]*)' pattern: '([^\"]*)'$")
+    public void verifyVariableDateWithPattern(String name, String date, String pattern) throws IOException {
+        assertThat(new DateTime(variables.get(name)), is(forPattern(pattern).parseDateTime(date)));
     }
 }
