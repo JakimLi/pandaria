e1cbe1fe14dac49aaf1667b1345975c8f0d1690a
jakimli <ljking7002@gmail.com>
2018-10-20 22:13:18 +0800

    add variable interept for mongo document

--- pandaria-mongo/src/main/java/com/github/jakimli/pandaria/steps/MongoSteps.java
+++ pandaria-mongo/src/main/java/com/github/jakimli/pandaria/steps/MongoSteps.java
@@ -1,6 +1,7 @@
 package com.github.jakimli.pandaria.steps;
 
 import com.github.jakimli.pandaria.domain.MongoClient;
+import com.github.jakimli.pandaria.domain.Variables;
 import cucumber.api.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,8 +10,11 @@ public class MongoSteps {
     @Autowired
     MongoClient mongo;
 
+    @Autowired
+    Variables variables;
+
     @When("^collection: '([^\"]*)' insert:$")
     public void insert(String collection, String document) {
-        mongo.insert(collection, document);
+        mongo.insert(collection, variables.interpret(document));
     }
 }
