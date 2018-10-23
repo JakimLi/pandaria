015bf978a8681f1e566b2db852c0cb8f0d570677
Jakim Li <jakimli@thoughtworks.com>
2018-10-23 17:43:04 +0800

    base uri be able to use variables

--- pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/FeatureConfigurationSteps.java
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/FeatureConfigurationSteps.java
@@ -1,6 +1,7 @@
 package com.github.jakimli.pandaria.steps;
 
 import com.github.jakimli.pandaria.domain.FeatureConfiguration;
+import com.github.jakimli.pandaria.domain.Variables;
 import cucumber.api.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,6 +10,9 @@ public class FeatureConfigurationSteps {
     @Autowired
     FeatureConfiguration configuration;
 
+    @Autowired
+    Variables variables;
+
     @Given("^dir: ([^\"]*)")
     public void dir(String dir) {
         configuration.dir(dir);
@@ -16,6 +20,6 @@ public class FeatureConfigurationSteps {
 
     @Given("^base uri: ([^\"]*)")
     public void baseUri(String uri) {
-        configuration.baseUri(uri);
+        configuration.baseUri(variables.interpret(uri));
     }
 }
