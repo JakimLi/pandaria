8efb05423ae4580c22df549c1e45edb1ce58f54f
jakimli <ljking7002@gmail.com>
2018-10-30 17:20:14 +0800

    optimize imports

--- pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/VariableDefinitionSteps.java
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/VariableDefinitionSteps.java
@@ -3,7 +3,6 @@ package com.github.jakimli.pandaria.steps;
 import com.github.jakimli.pandaria.domain.FeatureConfiguration;
 import com.github.jakimli.pandaria.domain.Variables;
 import com.github.jakimli.pandaria.domain.VerificationContext;
-import com.github.jakimli.pandaria.utils.FileUtil;
 import cucumber.api.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 
