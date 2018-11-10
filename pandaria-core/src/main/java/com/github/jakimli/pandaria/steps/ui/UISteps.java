68381998ca82030f5d23fb360efaa0fd820ec462
jakimli <ljking7002@gmail.com>
2018-11-10 17:09:56 +0800

    ui spike

---
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/ui/UISteps.java
@@ -0,0 +1,21 @@
+package com.github.jakimli.pandaria.steps.ui;
+
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class UISteps {
+    @When("^open: ([^\"]*)$")
+    public void openBrowserAndGo(String uri) {
+        ChromeOptions options = new ChromeOptions();
+
+        System.setProperty("webdriver.chrome.driver", "/Work/pandaria/pandaria-core/src/test/resources/chromedriver");
+
+        options.setHeadless(true);
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get(uri);
+    }
+}
---
+++ pandaria-core/src/test/resources/features/ui/open_browser_view_page.feature
@@ -0,0 +1,5 @@
+Feature: open browser
+  open a headless browser and then go to a page
+
+  Scenario: open browser and go to a page
+    * open: https://www.baidu.com
