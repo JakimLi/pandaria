8dbff38f185a886da9bd38d3121b0b4f09fe5b95
jakimli <ljking7002@gmail.com>
2018-11-10 23:59:03 +0800

    add text verification

---
+++ pandaria-core/src/main/java/com/github/jakimli/pandaria/steps/ui/TextVerification.java
@@ -0,0 +1,31 @@
+package com.github.jakimli.pandaria.steps.ui;
+
+import com.github.jakimli.pandaria.domain.ui.Driver;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TextVerification {
+
+    @Autowired
+    Driver driver;
+
+    @Then("verify: '([^\"]*)' text: '([^\"]*)'")
+    public void verifyTextById(String id, String text) {
+        WebElement element = driver.get().findElement(by(id));
+        assertThat(element.getText(), is(text));
+    }
+
+    private By by(String selector) {
+        if (selector.startsWith("#")) {
+            return By.id(selector.substring(1));
+        } else if (selector.startsWith(".")) {
+            return By.className(selector.substring(1));
+        }
+        throw new RuntimeException("invalid selector");
+    }
+}
--- pandaria-core/src/test/resources/features/ui/open_browser_view_page.feature
+++ pandaria-core/src/test/resources/features/ui/open_browser_view_page.feature
@@ -1,10 +1,10 @@
-@ignore
 Feature: open browser
   open a headless browser and then go to a page
 
   Background:
-    * base uri: http://locahost:10080
+    * base uri: http://localhost:12306
 
   Scenario: open browser and go to a page
     * open: /index.html
-#    * close
+    * verify: '#base' text: 'index.html'
+    * close
