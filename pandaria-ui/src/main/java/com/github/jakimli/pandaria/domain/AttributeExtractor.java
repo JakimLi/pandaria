472eda84e7cb603f4007c42e80b3d12f0bd142dd
jakimli <ljking7002@gmail.com>
2018-12-12 16:16:01 +0800

    refactoring

--- pandaria-ui/src/main/java/com/github/jakimli/pandaria/domain/AttributeExtractor.java
+++ pandaria-ui/src/main/java/com/github/jakimli/pandaria/domain/AttributeExtractor.java
@@ -11,18 +11,17 @@ public class AttributeExtractor {
 
     private WebElement element;
 
-    private Map<String, Extractor> extractors;
-
     public AttributeExtractor(WebElement element) {
         this.element = element;
-        this.extractors = of(
-                "selected", e -> String.valueOf(e.isSelected()),
-                "enabled", e -> String.valueOf(e.isEnabled())
-        );
     }
 
     public String attribute(String name) {
-        return Optional.ofNullable(this.extractors.get(name))
+        Map<String, Extractor> extractors = of(
+                "selected", e -> String.valueOf(e.isSelected()),
+                "enabled", e -> String.valueOf(e.isEnabled())
+        );
+
+        return Optional.ofNullable(extractors.get(name))
                 .map(e -> e.extract(element))
                 .orElse(element.getAttribute(name));
     }
